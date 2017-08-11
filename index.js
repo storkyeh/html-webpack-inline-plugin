b00720da67d5dfbdba76fe94fbfad844b2bedb63
storkyeh <storkyeh@gmail.com>
2017-08-11 10:15:11 +0800

    use inline-source

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-var fs = require('fs')
-
-var inlineReg = /<!-- inline-plugin[\w\W\r\n]*?-->/g
-var pathReg = /path="(.*?)"/
-
-function wrapScript(script) {
-  return '<script>' + script + '</script>\n'
-}
+var inline = require('inline-source')
 
 function HtmlWebpackInlinePlugin(options) {
   // Setup the plugin instance with options...
@@ -13,23 +6,18 @@ function HtmlWebpackInlinePlugin(options) {
 }
 
 HtmlWebpackInlinePlugin.prototype.apply = function(compiler) {
-    compiler.plugin('compilation', function(compilation, options) {
-        compilation.plugin('html-webpack-plugin-before-html-processing', function(htmlPluginData, callback) {
-          var html = htmlPluginData.html
-          var tags = html.match(inlineReg)
-          if (tags) {
-            tags.forEach(function(tag) {
-              var ret = tag.match(pathReg)
-              if (ret && ret[1]) {
-                var scriptContent = fs.readFileSync(ret[1], 'utf8')
-                html = html.replace(tag, wrapScript(scriptContent))
-              }
-            })
-            htmlPluginData.html = html
-          }
-          callback(null, htmlPluginData)
-        });
+  var self = this
+  compiler.plugin('compilation', function(compilation, options) {
+    compilation.plugin('html-webpack-plugin-before-html-processing', function(htmlPluginData, callback) {
+      var html = htmlPluginData.html
+      inline(html, self.options, function(err, html) {
+        if (!err) {
+          htmlPluginData.html = html
+        }
+        callback(null, htmlPluginData)
+      })
     });
+  });
 };
 
 module.exports = HtmlWebpackInlinePlugin
